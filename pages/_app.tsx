ee5433a9786b7332b94bb7e1788145dfae6156d2
Elizabeth Eidelson <fluturecode@gmail.com>
2022-06-27 17:04:46 -0400

    modified _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import type { AppProps } from 'next/app'
 import { GlobalContext } from "../context";
 import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export const MyApp = ({ Component, pageProps }: AppProps) => {
   const [network, setNetwork] = useState<Cluster | undefined>("devnet");
   const [account, setAccount] = useState<Keypair | null>(null);
   const [mnemonic, setMnemonic] = useState<string | null>(null);
@@ -15,9 +15,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GlobalContext.Provider value={{ network, setNetwork, account, setAccount, mnemonic, setMnemonic, balance, setBalance }}>
       <Layout>
-        <Component {...pageProps} />
+        <div>props</div>
       </Layout>
     </GlobalContext.Provider>
   )
 }
-export default MyApp
